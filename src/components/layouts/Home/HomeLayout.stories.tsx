0eef5eaa9477dbaece04157263d7f047562af1f2
Murilo Martins <murilolmartins1@gmail.com>
2023-06-28 12:05:55 -0300

    feat(home-story): remove provider

--- src/components/layouts/Home/HomeLayout.stories.tsx
+++ src/components/layouts/Home/HomeLayout.stories.tsx
@@ -1,29 +1,10 @@
-import { videoListValues } from '@constants';
-import { VideoListProvider } from '@contexts';
-
 import { HomeLayout } from './HomeLayout';
 
 import type { Meta, StoryObj } from '@storybook/react';
-import { sortArray } from '@utils';
 
 const meta: Meta<typeof HomeLayout> = {
   title: 'Layouts/HomeLayout',
-  component: HomeLayout,
-  decorators: [
-    (Story) => {
-      return (
-        <VideoListProvider
-          initialData={{
-            search: sortArray(videoListValues, 'createdAt'),
-            initialData: videoListValues
-          }}
-          initialLoading={false}
-        >
-          <Story />
-        </VideoListProvider>
-      );
-    }
-  ]
+  component: HomeLayout
 };
 
 type Story = StoryObj<typeof HomeLayout>;
